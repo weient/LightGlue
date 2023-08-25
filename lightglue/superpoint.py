e461a36e30d514701895be85bf21e95faaab084e
weient <67552109+weient@users.noreply.github.com>
2023-08-25 15:11:05 +0800

    Update superpoint.py

--- lightglue/superpoint.py
+++ lightglue/superpoint.py
@@ -210,7 +210,7 @@ class SuperPoint(nn.Module):
         # Extract descriptors
         descriptors = [sample_descriptors(k[None], d[None], 8)[0]
                        for k, d in zip(keypoints, descriptors)]
-        print('d:', descriptors)
+        print('d:', descriptors[0].shape)
         return {
             'keypoints': torch.stack(keypoints, 0),
             'keypoint_scores': torch.stack(scores, 0),
