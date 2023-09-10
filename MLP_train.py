a5b57b9eb2c7af584a14468d80db371f3a33fe4f
weient <stephanie20001118@gmail.com>
2023-09-10 22:19:58 +0800

    change dataset loading

--- MLP_train.py
+++ MLP_train.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from torch.utils.tensorboard.writer import SummaryWriter
 import torch.nn.functional as F
 from datetime import datetime
-
+import itertools
 
 def sigmoid_log_double_softmax(
         sim: torch.Tensor, z0: torch.Tensor, z1: torch.Tensor) -> torch.Tensor:
@@ -65,21 +65,35 @@ class MLP_module(nn.Module):
 class MLPDataset(Dataset):
 
     # data loading
-    def __init__(self, index):
-        self.data_path = '/mnt/home_6T/public/weien/MLP_data/room'+str(index)+'.pt'
-        self.data = torch.load(self.data_path)
+    def __init__(self):
+        self.data_root = './tmp5/'
+        # self.data_path = '/mnt/home_6T/public/weien/MLP_data/room'+str(index)+'.pt'
+        nums = [str(i) for i in range(1, 21)]
+        r = 2
+        combinations = list(itertools.combinations(nums, r))
+        self.data_comb = []
+        for i in range(1, 16):
+            if i==4:
+                continue
+            for j in combinations:
+                self.data_comb.append([str(i), j[0], j[1]])
+        # self.data = torch.load(self.data_path)
     # working for indexing
     def __getitem__(self, index):
-        return self.data[index][0], self.data[index][1], self.data[index][2]
+        img0 = load_image(self.data_root+self.data_comb[index][0]+'.'+self.data_comb[index][1]+'.png')
+        img1 = load_image(self.data_root+self.data_comb[index][0]+'.'+self.data_comb[index][2]+'.png')
+        
+        return img0, img1
     # return the length of our dataset
     def __len__(self):
-        return len(self.data)
+        return len(self.data_comb)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
 model = MLP_module().to(device)
 loss_fn = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.00001, momentum=0.9)
-
+extractor = SuperPoint(max_num_keypoints=2048).eval().to(device)  # load the extractor
+matcher = LightGlue(features='superpoint').eval().to(device)
 
 def train_one_epoch(epoch_index, tb_writer):
     running_loss = 0.
@@ -87,8 +101,12 @@ def train_one_epoch(epoch_index, tb_writer):
 
     for i, data in enumerate(train_loader):
         # Every data instance is an input + label pair
-        input0, input1, label = data
-
+        img0, img1 = data
+        with torch.no_grad():
+            feats0 = extractor.extract(img0.clone().detach().to(device))
+            feats1 = extractor.extract(img1.clone().detach().to(device))
+            matches01 = matcher({'image0': feats0, 'image1': feats1})
+            input0, input1, label = matches01['input0'], matches01['input1'], matches01['label']
         # Zero your gradients for every batch!
         optimizer.zero_grad()
 
@@ -105,8 +123,8 @@ def train_one_epoch(epoch_index, tb_writer):
         # Gather data and report
         running_loss += loss.item()
         #print(loss.item())
-        if i % 10 == 9:
-            last_loss = running_loss / 10 # loss per batch
+        if i % 100 == 99:
+            last_loss = running_loss / 100 # loss per batch
             print('  batch {} loss: {}'.format(i + 1, last_loss))
             tb_x = epoch_index * len(train_loader) + i + 1
             tb_writer.add_scalar('Loss/train', last_loss, tb_x)
@@ -123,49 +141,53 @@ epoch_number = 0
 EPOCHS = 500
 
 best_vloss = 1_000_000.
-num_pt = 15
+seed = 100
+torch.manual_seed(seed)
+dataset = MLPDataset()
+train_set, val_set = random_split(dataset, [0.8, 0.2])
+train_loader = DataLoader(train_set, batch_size=1, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=1, shuffle=True)
 for epoch in range(EPOCHS):
     print('EPOCH {}:'.format(epoch_number + 1))
 
     # Make sure gradient tracking is on, and do a pass over the data
-    for num in range(num_pt):
-        if num+1 == 4:
-            continue
-        dataset = MLPDataset(num+1)
-        train_set, val_set = random_split(dataset, [0.8, 0.2])
-        train_loader = DataLoader(train_set, batch_size=1, shuffle=True)
-        val_loader = DataLoader(val_set, batch_size=1, shuffle=True)
-        model.train(True)
-        avg_loss = train_one_epoch(epoch_number, writer)
+    
+
+    model.train(True)
+    avg_loss = train_one_epoch(epoch_number, writer)
 
 
-        running_vloss = 0.0
-        # Set the model to evaluation mode, disabling dropout and using population
-        # statistics for batch normalization.
-        model.eval()
+    running_vloss = 0.0
+    # Set the model to evaluation mode, disabling dropout and using population
+    # statistics for batch normalization.
+    model.eval()
 
-        # Disable gradient computation and reduce memory consumption.
-        with torch.no_grad():
-            for i, vdata in enumerate(val_loader):
-                vin0, vin1, vlabels = vdata
-                voutputs = model(vin0, vin1)
-                vloss = loss_fn(voutputs, vlabels)
-                running_vloss += vloss
+    # Disable gradient computation and reduce memory consumption.
+    with torch.no_grad():
+        for i, vdata in enumerate(val_loader):
+            vimg0, vimg1 = vdata
+            vfeats0 = extractor.extract(vimg0.to(device))
+            vfeats1 = extractor.extract(vimg1.to(device))
+            vmatches01 = matcher({'image0': vfeats0, 'image1': vfeats1})
+            vin0, vin1, vlabels = vmatches01['input0'], vmatches01['input1'], vmatches01['label']
+            voutputs = model(vin0, vin1)
+            vloss = loss_fn(voutputs, vlabels)
+            running_vloss += vloss
 
-        avg_vloss = running_vloss / (i + 1)
-        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+    avg_vloss = running_vloss / (i + 1)
+    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
 
-        # Log the running loss averaged per batch
-        # for both training and validation
-        writer.add_scalars('Training vs. Validation Loss',
-                        { 'Training' : avg_loss, 'Validation' : avg_vloss },
-                        epoch_number+1)
-        writer.flush()
+    # Log the running loss averaged per batch
+    # for both training and validation
+    writer.add_scalars('Training vs. Validation Loss',
+                    { 'Training' : avg_loss, 'Validation' : avg_vloss },
+                    epoch_number+1)
+    writer.flush()
 
-        # Track best performance, and save the model's state
-        if avg_vloss < best_vloss:
-            best_vloss = avg_vloss
-            model_path = '/mnt/home_6T/public/weien/MLP_checkpoint/model_{}_{}'.format(timestamp, epoch_number)
-            torch.save(model.state_dict(), model_path)
+    # Track best performance, and save the model's state
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
+        model_path = '/mnt/home_6T/public/weien/MLP_checkpoint/model_{}_{}'.format(timestamp, epoch_number)
+        torch.save(model.state_dict(), model_path)
 
     epoch_number += 1
